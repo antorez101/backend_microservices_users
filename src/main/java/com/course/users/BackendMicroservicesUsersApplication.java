c18c10e535013fd8e39458184aefb28abcae59a7
Marko <sihedo101@gmail.com>
2020-10-07 21:37:28 -0500

    Repository creation

---
+++ src/main/java/com/course/users/BackendMicroservicesUsersApplication.java
@@ -0,0 +1,13 @@
+package com.course.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMicroservicesUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMicroservicesUsersApplication.class, args);
+	}
+
+}
